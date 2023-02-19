d5ef6ef5043f9ce9d86f25f6c69816265c94101a
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-02-20 01:01:41 +0300

    sql-organising-bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 45, 'M', 'White', 'hostile', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 23, 'F', 'black', 'calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 16, 'F', 'White', 'calm', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sargeant Brown', 25, 'M', 'Black', 'calm', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 21, 'F', 'dark brown', 'calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 16, 'M', 'Black', 'calm', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 14, 'F', 'white', 'aggressive', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 29, 'M', 'brown', 'calm', 0);
